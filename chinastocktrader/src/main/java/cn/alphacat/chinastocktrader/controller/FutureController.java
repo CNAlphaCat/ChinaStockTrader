83620070993216726e7db46caf93f6ad253b9fa2
CNLyon <alphacat1024@163.com>
2025-04-28 12:53:12 +0800

    new feature controller for im/if

--- chinastocktrader/src/main/java/cn/alphacat/chinastocktrader/controller/FutureController.java
+++ chinastocktrader/src/main/java/cn/alphacat/chinastocktrader/controller/FutureController.java
@@ -1,25 +1,35 @@
 package cn.alphacat.chinastocktrader.controller;
 
-import cn.alphacat.chinastockdata.future.FutureService;
 import cn.alphacat.chinastockdata.model.FutureMarketOverview;
+import cn.alphacat.chinastocktrader.service.ChinaStockTraderFutureService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/feature")
+@RequestMapping("/api/future")
 public class FutureController {
-  private final FutureService futureService;
+  private final ChinaStockTraderFutureService chinaStockTraderFutureService;
 
-  public FutureController(final FutureService futureService) {
-    this.futureService = futureService;
+  public FutureController(final ChinaStockTraderFutureService chinaStockTraderFutureService) {
+    this.chinaStockTraderFutureService = chinaStockTraderFutureService;
   }
 
+  /*
+   * 获取所有期货品种信息
+   */
   @RequestMapping("/futuresBaseInfo")
   public List<FutureMarketOverview> getFuturesBaseInfo() {
-    return futureService.getFuturesBaseInfo();
+    return chinaStockTraderFutureService.getFuturesBaseInfo();
   }
 
-
+  /*
+   * 获取IM和IF的比值
+   */
+  @RequestMapping("/currentIMDivideIFPrice")
+  public BigDecimal getCurrentIMDivideIFPrice() {
+    return chinaStockTraderFutureService.getCurrentIMDivideIFPrice();
+  }
 }
---
+++ chinastocktrader/src/main/java/cn/alphacat/chinastocktrader/service/ChinaStockTraderFutureService.java
@@ -0,0 +1,40 @@
+package cn.alphacat.chinastocktrader.service;
+
+import cn.alphacat.chinastockdata.future.FutureService;
+import cn.alphacat.chinastockdata.model.FutureHistory;
+import cn.alphacat.chinastockdata.model.FutureMarketOverview;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.List;
+
+@Service
+public class ChinaStockTraderFutureService {
+  private final FutureService featureService;
+  private final IFFutureService ifFutureService;
+  private final IMFutureService imFutureService;
+
+  public ChinaStockTraderFutureService(
+      final FutureService featureService,
+      final IFFutureService ifFutureService,
+      final IMFutureService imFutureService) {
+    this.featureService = featureService;
+    this.ifFutureService = ifFutureService;
+    this.imFutureService = imFutureService;
+  }
+
+  public List<FutureMarketOverview> getFuturesBaseInfo() {
+    return featureService.getFuturesBaseInfo();
+  }
+
+  public BigDecimal getCurrentIMDivideIFPrice() {
+    List<FutureHistory> imHistoryList = imFutureService.getTodayFutureHistory();
+    List<FutureHistory> ifHistoryList = ifFutureService.getTodayFutureHistory();
+    FutureHistory imHistory = imHistoryList.getLast();
+    FutureHistory ifHistory = ifHistoryList.getLast();
+    BigDecimal imPrice = imHistory.getClose();
+    BigDecimal ifPrice = ifHistory.getClose();
+    return imPrice.divide(ifPrice, 4, RoundingMode.HALF_UP);
+  }
+}
